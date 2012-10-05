7d01e8745112db0d07cf240f56590383ce0b153e
jaymyers <jay.myers@bestbuy.com>
2012-10-05 13:28:00 -0500

    changes to RequestStore.java to capture strings and pass them to MongoDB

--- src/main/java/com/bbyopen/request/store/RequestStore.java
+++ src/main/java/com/bbyopen/request/store/RequestStore.java
@@ -1,5 +1,6 @@
 package com.bbyopen.request.store;
 
+import org.netkernel.layer0.nkf.INKFRequest;
 import org.netkernel.layer0.nkf.INKFRequestContext;
 import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
 
@@ -7,9 +8,19 @@ public class RequestStore extends StandardAccessorImpl{
 	
 	@Override
 	public void onSource(INKFRequestContext context) throws Exception{
-		String url = context.getThisRequest().getArgumentValue("url");
+		String data = context.source("arg:data", String.class);
+
+		INKFRequest storeRequest = context.createRequest("active:mongoDB");
+		storeRequest.addArgumentByValue("dbserver","localhost:27017");
+		storeRequest.addArgumentByValue("dbname", "test");
+		storeRequest.addArgumentByValue("collection", "test");
+		storeRequest.addArgumentByValue("action", "insert");
+		storeRequest.addArgumentByValue("data",data);
+		
+		context.issueRequest(storeRequest);
+		
 		// performing sys out, ideally we would store data in a nosql store here
-		System.out.println("Captured url = " + url);
-		context.createResponseFrom(url);
+		System.out.println("request = " + data);
+//		context.createResponseFrom(data);
 	}
 }
